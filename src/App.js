0b4fba9db0d81e5ce7c6d5347ff314439d8d714c
Kamandla Balashekhar <60310860+BALASHEKHAR@users.noreply.github.com>
2020-09-04 19:22:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
 import React from 'react';
+import Favourite from './components/favourite';
+import Home from './components/Home';
+import Settings from './components/Settings';
+import ErrorPage404 from './components/ErrorPage404';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import PasswordSent from './components/passwordSent'
+import Auth from './components/auth';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+ 
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1> First React Deploy in Github </h1>
-      <p> Instead, it will copy all the configuration files 
-      and the transitive dependencies (webpack, Babel, ESLint, 
-      etc) right into your project so you have full control over
-       them. All of the commands except eject will still work, 
-       but they will point to the copied scripts so you can tweak
-       them. At this point you’re on your own.
-       </p>
-       
+     <Router>
+     <Switch>
+
+      <Route path="/home">
+      <Auth>
+      <Home/>
+      </Auth>
+      </Route>
+
+      <Route path="/forget_password">
+      <PasswordSent/>
+      </Route>
+
+      <Route path="/settings">
+      <Settings/>
+      </Route>
+
+      <Route path="/favourite">
+      <Favourite/>
+      </Route>
+
+      <Route exact path="/Login">
+      <Auth nonAuth={true}>
+      <Login />
+      </Auth>
+      </Route>
+      <Route exact path="/signup">
+      <Auth nonAuth={true}>
+      <Signup/>
+      </Auth>
+      </Route>
+      <Route path="*" 
+      render={()=><ErrorPage404 />}
+      />
+      </Switch>
+     </Router>
     </div>
   );
 }
