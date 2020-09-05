95369ba64a266656e204f901259722e6350614ea
Kamandla Balashekhar <60310860+BALASHEKHAR@users.noreply.github.com>
2020-09-05 07:11:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1> First React Deploy in Github </h1>
-      <p>  into your project so you have full control over
+      <p>  hi there these is my first into your project so you have full control over
        them. All of the commands except eject will still work, 
        but they will point to the copied scripts so you can tweak
        them. At this point you’re on your own.
